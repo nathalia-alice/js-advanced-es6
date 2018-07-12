920c6e3fa00c2d9744beff26505d0ab43350e988
nathalia-silva <nathalia.alice.silv@gmail.com>
2018-07-12 15:48:46 -0300

    Padrão de Projeto Factory

---
+++ client/js/app/services/ProxyFactory.js
@@ -0,0 +1,18 @@
+class ProxyFactory{
+    static create(objeto, props, acao){
+        return new Proxy(new ListaNegociacoes(), {
+
+            get(target, prop, receiver) {
+                if (props.includes(prop) && typeof (target[prop]) == typeof (Function)) {
+                    return function () {
+                        console.log(`a propriedade "${prop}" foi interceptada`);
+                        Reflect.apply(target[prop], target, arguments);
+                        return acao(target);
+
+                    }
+                }
+                return Reflect.get(target, prop, receiver);
+            }
+        });
+    }
+}
