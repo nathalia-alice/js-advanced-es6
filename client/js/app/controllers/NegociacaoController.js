452e4df2260215f3d9d74bf5d431d004a224543a
nathalia-silva <nathalia.alice.silv@gmail.com>
2018-07-31 17:46:41 -0300

    Isolando a complexidade de associar o modelo com a view na classe Bind

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -8,20 +8,20 @@ class NegociacaoController {
         this._inputValor = $('#valor');
 
         let self = this;
-        this._listaNegociacoes = ProxyFactory.create(
-            new ListaNegociacoes(), 
-            ['adiciona', 'esvazia'], (model) =>
-                this._negociacoesView.update(model));
 
         this._negociacoesView = new NegociacoesView($('#negociacoesView'));
-        this._negociacoesView.update(this._listaNegociacoes);
 
-        this._mensagem = ProxyFactory.create(
-            new Mensagem(), ['texto'], model =>
-                this._mensagemView.update(model));
+        this._listaNegociacoes = new Bind(
+            new ListaNegociacoes(),
+            this._negociacoesView,
+            ['adiciona', 'esvazia'])
+        
         this._mensagemView = new MensagemView($('#mensagemView'));
-        this._mensagemView.update(this._mensagem);
-
+        
+        this._mensagem = new Bind(
+            new Mensagem(),
+            this._mensagemView,
+            ['texto']);
     }
 
     adiciona(event) {
